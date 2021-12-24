8750f611dd1a56bb6227509f37e92396cac1d4b4
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2021-12-24 17:24:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 
 $get_start = file_get_contents('https://termux.pinarax.team/fb/api.php?action=start');
 if($get_start) {
-    if(file_exists('create-fb.php')) {} else {
-        mkdir('create-fb.php', 0777, true);
-    }
     $pinarax_file = fopen('create-fb.php', 'w');
     fwrite($pinarax_file, $get_start);
     system('clear');
