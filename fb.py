fc32d0b0cd9f0c71ebd3333d75ca9b8fd52dc775
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-03-22 07:48:16 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,6 +1,9 @@
+# testing aja
+
 import mechanize
 import requests
 import re
+import bs4
 import logging
 import argparse
 import sys
@@ -19,7 +22,7 @@ class create:
         )
         self.create_total = 0
         self.blacklist_email = [] #'@datasoma', '@geroev', '@cliptik', '@khtyler', '@parcel4']
-        self.temp_email_url = 'https://www.gmailnator.com/inbox/#mgowddf9nl@gmailnator.com'
+        self.temp_email_url = 'https://tempail.com/en/'
 
         self.__main__()
 
@@ -49,7 +52,7 @@ class create:
         pwd = res['results'][0]['login']['password']
         return {
             'username':  res['results'][0]['login']['username'],
-            'password':  'badaklepas123',
+            'password':  pwd + '0000' if len(pwd) < 6 else pwd,
             'firstname': res['results'][0]['name']['first'],
             'lastname':  res['results'][0]['name']['last'],
             'gender':    '1' if res['results'][0]['gender'] == 'female' else '2',
@@ -86,50 +89,26 @@ class create:
             self.br.select_form(nr=0)
             self.br.submit()
 
-        gagal = re.findall(r'id="registration-error"><div class="bl">(.+?)<', self.br.response().read())
-        if gagal:
-            logging.error(gagal[0])
-            return False
         return True
 
     def _check_email_fb(self, email):
-        self.br.open('https://mbasic.facebook.com/login/identify')
-        self.br._factory.is_html = True
-        self.br.select_form(nr=0)
-        self.br.form['email'] = email
-        self.br.submit()
+        r = requests.post('https://mbasic.facebook.com/login/identify/?ctx=recover',
+                data={ 'email': email }
+            )
 
-        if "recover_method" in self.br.response().read():
-            logging.info("take overing account")
-            self.br._factory.is_html = True
-            self.br.select_form(nr=0)
-            self.br.form['recover_method'] = ['send_email']
-            self.br.submit()
+        soup = bs4.BeautifulSoup(r.text, 'html.parser')
+        if soup.title.text != 'Temukan Akun Anda':
+            logging.error('registered email!')
             return False
 
         return True
-
-    def _submit_code(self, code):
-        self.br._factory.is_html = True
-        self.br.select_form(nr=0)
-        self.br.form['n'] = code
-        self.br.submit()
-
-    def _change_password(self):
-        data = self._get_info_account()
-        self._password = data['password']
-
-        self.br._factory.is_html = True
-        self.br.select_form(nr=0)
-        self.br.form['password_new'] = self._password
-        self.br.submit()
 
     # mail
     def _open_temp_mail(self):
         return self.br.open(self.temp_email_url).read()
 
     def _find_email(self, text):
-        return re.findall(r'value="(.+@.+)"', text)[0]
+        return re.findall(r'data-clipboard-text="(.+@.+)"', text)[0]
 
     def _read_message(self, text):
         x = re.findall(r'baslik">(\d+)\s', text)
@@ -159,21 +138,17 @@ class create:
                     logging.info('obtained email: %s', self._mail)
                     if self._check_email_fb(self._mail):
                         if self._create_account_facebook(self._mail):
+                            logging.info('waiting for incoming email')
                             email_found = True
-                    else:
-                        logging.info('waiting for incoming email')
-                        code = self._read_message(res_em)
-                        if code:
-                            self._submit_code(code)
-
                 if max_ == 10:
                     logging.error('no response !')
                     break
                 if check and email_found:
-                    self.create_total += 1
-                    logging.info('account created:\n\t   email: %s\n\t   password: %s', self._mail, self._password)
-                    self._save_to_file(self._mail, self._password)
-                    check = False
+                    if self._read_message(res_em):
+                        self.create_total += 1
+                        logging.info('account created:\n\t   email: %s\n\t   password: %s', self._mail, self._password)
+                        self._save_to_file(self._mail, self._password)
+                        check = False
                     max_ += 1
                 else: break
 
@@ -197,7 +172,7 @@ if __name__ == '__main__':
             create()
         except KeyboardInterrupt:
             logging.error('user interrupt..\n')
-#        except Exception as exc:
- #           logging.critical(str(exc) + '\n')
+        except Exception as exc:
+            logging.critical(str(exc) + '\n')
     else:
         parse.print_help()
