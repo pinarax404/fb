6b6c008d5538485554288ccb33860bc951b71bc8
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-07 04:25:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ function php_ajax($mail, $name) {
     $data = 'lsd=AVrxld3BMYY&jazoest=2965&ccp=2&reg_instance=HI11Ypj22hrTxWLR734IvG6A&submission_request=true&helper=&reg_impression_id=839c9f18-db27-44b8-8244-b38e9098f0ae&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname='.$name.'&reg_email__='.$mail.'&sex=1&custom_gender=&did_use_age=false&birthday_day=6&birthday_month=5&birthday_year=1999&age_step_input=&reg_passwd__=badaklepas&submit=Sign+Up';
     
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://mbasic.facebook.com/reg/submit/?cid=103');
+    curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/reg/submit/?cid=103');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'content-type: application/x-www-form-urlencoded',
         'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
