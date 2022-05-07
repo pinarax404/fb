e51903a19663e1394a33dcaacdfd20f966378a01
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 04:20:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,7 @@ $useragent = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) Ap
 //********************//
 
 
-    $get_ip     = curl_attr('https://ipwhois.app/json/', false, false);
-    $get_attr   = curl_attr('https://randomuser.me/api/?gender=female&nat=us', false, false);
-    $get_email  = curl_attr('http://ese.kr/?pb=6549', false, false);
-
+    $get_ip     = curl_attr('https://ipwhois.app/json/', false, false, false, false);
     if($get_ip !== false) {
         $res_ip = json_decode($get_ip, true);
         echo "\033[1;37mIP : " . $res_ip['ip'] . " | Country : " . $res_ip['country'] . "\033[1;37m\n";
@@ -26,6 +23,10 @@ $useragent = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) Ap
         echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
         echo "\033[1;37m========================================\033[1;37m\n";
     }
+
+
+    $get_attr   = curl_attr('https://randomuser.me/api/?gender=female&nat=us', false, false, false, false);
+    $get_email  = curl_attr('http://ese.kr/?pb=6549', false, false, false, false);
 
     if($get_attr !== false && $get_email !== false && strpos($get_email, 'name="mailbox"') !== false) {
         $res_attr   = json_decode($get_attr, true);
@@ -37,9 +38,12 @@ $useragent = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) Ap
         echo "\033[1;37m◆ Email     : " . $email . "\033[1;37m\n";
         echo "\033[1;37m◆ Creating  : ";
 
-        $data_post_create_1 = 'lsd=AVrzaUa2khY&jazoest=21016&ccp=2&reg_instance=&submission_request=true&helper=&reg_impression_id=818c417c-2220-427e-91a5-1aad57f1b6c4&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname='.$first_name.'+&lastname='.$first_name.'&reg_email__='.$email.'&sex=1&custom_gender=&did_use_age=false&birthday_month=5&birthday_day=12&birthday_year=1999&age_step_input=&reg_passwd__='.$default_password.'&submit=Sign Up';
-        $post_create_1 = curl_attr('https://mbasic.facebook.com/reg/submit/?cid=103', $data_post_create_1, $useragent);
-        echo "\n" . $post_create_1;
+        $get_fb_cred = curl_attr('https://mbasic.facebook.com/reg/submit/', false, $useragent, true, false);
+        if($get_fb_cred !== false  && strpos($get_fb_cred, 'method="post" action="/cookie/consent/') !== false) {
+            echo "\033[1;31mProxy Cookies Error\033[1;37m\n";
+        } else {
+            echo "\n" . $get_fb_cred;
+        }
         
     } else {
         echo 'looping';
@@ -52,7 +56,7 @@ function replace_string($start, $end, $data) {
     return $rt;
 }
 
-function curl_attr($url, $body, $useragent) {
+function curl_attr($url, $body, $useragent, $createcookies, $readcookies) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -69,7 +73,12 @@ function curl_attr($url, $body, $useragent) {
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    //curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookiesfb.txt');
+    if($createcookies) {
+        curl_setopt($ch, CURLOPT_COOKIEJAR, 'tmp.txt');
+    }
+    if($readcookies) {
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'tmp.txt');
+    }
     $respons_data = curl_exec($ch);
     $respons_header = substr($respons_data, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
     $respons_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
