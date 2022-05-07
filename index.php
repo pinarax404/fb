3911866db66bc2b966f8d47f109015b8ed770da2
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 02:15:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
 system('clear');
 
 $getip = file_get_contents('https://ipwhois.app/json/');
+$getattr = file_get_contents('https://randomuser.me/api/?gender=female&nat=us');
+
+
+
 if($getip && strpos($getip, 'ip') !== false && strpos($getip, 'country') !== false) {
     $res_ip = json_decode($getip, true);
     echo "\033[1;37mIP : " . $res_ip['ip'] . " | Country : " . $res_ip['country'] . "\033[1;37m\n";
 } else {
     echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
+}
+
+
+if($getattr && strpos($getattr, 'name') !== false && strpos($getattr, 'first') !== false && strpos($getattr, 'last') !== false) {
+    $res_attr = json_decode($getattr, true);
+    $first_name = strtolower($json_generate_user['results']['0']['name']['first'];
+    $last_name = $json_generate_user['results']['0']['name']['last'];
+} else {
+    
 }
