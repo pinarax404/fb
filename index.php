68177eba7d87a26c3dfa027d563526044aa5e3c4
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 04:08:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,8 +37,8 @@ $useragent = "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) Ap
         echo "\033[1;37m◆ Email     : " . $email . "\033[1;37m\n";
         echo "\033[1;37m◆ Creating  : ";
 
-        $data_post_create_1 = 'lsd=AVrzaUa2khY&jazoest=21016&ccp=2&reg_instance=Xi92YmDws84GGNBn4Lqqfmdb&submission_request=true&helper=&reg_impression_id=818c417c-2220-427e-91a5-1aad57f1b6c4&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname='.$first_name.'+&lastname='.$first_name.'&reg_email__='.$email.'&sex=1&custom_gender=&did_use_age=false&birthday_month=5&birthday_day=7&birthday_year=2002&age_step_input=&reg_passwd__='.$default_password.'&submit=Sign+Up';
-        $post_create_1 = curl_attr('https://mbasic.facebook.com/reg/submit/?cid=103', $data_post_create_1, $useragent);
+        $data_post_create_1 = 'lsd=AVrzaUa2khY&jazoest=21016&ccp=2&reg_instance=&submission_request=true&helper=&reg_impression_id=818c417c-2220-427e-91a5-1aad57f1b6c4&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname='.$first_name.'+&lastname='.$first_name.'&reg_email__='.$email.'&sex=1&custom_gender=&did_use_age=false&birthday_month=5&birthday_day=7&birthday_year=2002&age_step_input=&reg_passwd__='.$default_password.'&submit=Sign+Up';
+        $post_create_1 = curl_attr('https://mobile.facebook.com/reg/submit/?cid=103', $data_post_create_1, $useragent);
         echo "\n" . $post_create_1;
         
     } else {
@@ -56,7 +56,9 @@ function curl_attr($url, $body, $useragent) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        //'content-type: application/x-www-form-urlencoded',
+        'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language: en-US,en;q=0.9',
+        'content-type: application/x-www-form-urlencoded',
         'user-agent: ' . $useragent
     ));
     if($body) {
