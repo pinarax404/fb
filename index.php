8a2ad3138326f8bba784c3bd0b00e729c332a40e
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 02:35:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,24 +3,59 @@
 //ini_set('display_errors', "0");
 system('clear');
 
-$getip = file_get_contents('https://ipwhois.app/json/');
-$getattr = file_get_contents('https://randomuser.me/api/?gender=female&nat=us');
+soot_start();
+
+function soot_start() {
+    $get_ip     = curl_attr('https://ipwhois.app/json/', false);
+    $get_attr   = curl_attr('https://randomuser.me/api/?gender=female&nat=us', false);
+    $get_email  = curl_attr('http://ese.kr/?pb=6549', false);
 
 
+    if($get_ip !== false) {
+        $res_ip = json_decode($get_ip, true);
+        echo "\033[1;37mIP : " . $res_ip['ip'] . " | Country : " . $res_ip['country'] . "\033[1;37m\n";
+    } else {
+        echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
+    }
 
-if($getip && strpos($getip, 'ip') !== false) {
-    $res_ip = json_decode($getip, true);
-    echo "\033[1;37mIP : " . $res_ip['ip'] . " | Country : " . $res_ip['country'] . "\033[1;37m\n";
-} else {
-    echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
+    if($get_attr !== false && $get_email !== false && strpos($get_email, 'name="mailbox"') !== false) {
+        $res_attr   = json_decode($get_attr, true);
+        $first_name = strtolower($json_generate_user['results']['0']['name']['first']);
+        $last_name  = strtolower($json_generate_user['results']['0']['name']['last']);
+        $email      = replace_string('<input type="search" name="mailbox" value="', '"', $get_email);
+
+        echo "\033[1;37mFull Name : " . $first_name . ' ' . $last_name . "\n";
+        echo "\033[1;37mEmail : " . $email . "\n";
+        echo "\033[1;37mCreating Account...\n";
+
+    }
 }
 
+function replace_string($start, $end, $data) {
+    $rt = explode($start, $data)[1];
+    $rt = explode($end, $rt)[0];
+    return $rt;
+}
 
-if($getattr && strpos($getattr, 'name') !== false) {
-    $res_attr = json_decode($getattr, true);
-    $first_name = strtolower($json_generate_user['results']['0']['name']['first']);
-    $last_name = strtolower($json_generate_user['results']['0']['name']['last']);
-    echo $first_name . ' ' . $last_name . "\n";
-} else {
-    echo "Failed\n";
+function curl_attr($url, $body) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if($body) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+    }
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $respons_data = curl_exec($ch);
+    $respons_header = substr($respons_data, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $respons_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if($respons_http_code == 200) {
+        return $respons_data;
+    } else {
+        return false;
+    }
 }
