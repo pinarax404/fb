641faa3c9dc2deccda548d450e07162141567193
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 02:17:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $getattr = file_get_contents('https://randomuser.me/api/?gender=female&nat=us');
 
 
 
-if($getip && strpos($getip, 'ip') !== false && strpos($getip, 'country') !== false) {
+if($getip && strpos($getip, 'ip') !== false) {
     $res_ip = json_decode($getip, true);
     echo "\033[1;37mIP : " . $res_ip['ip'] . " | Country : " . $res_ip['country'] . "\033[1;37m\n";
 } else {
@@ -16,10 +16,11 @@ if($getip && strpos($getip, 'ip') !== false && strpos($getip, 'country') !== fal
 }
 
 
-if($getattr && strpos($getattr, 'name') !== false && strpos($getattr, 'first') !== false && strpos($getattr, 'last') !== false) {
+if($getattr && strpos($getattr, 'name') !== false) {
     $res_attr = json_decode($getattr, true);
     $first_name = strtolower($json_generate_user['results']['0']['name']['first'];
     $last_name = $json_generate_user['results']['0']['name']['last'];
+    echo $first_name . ' ' . $last_name . "\n";
 } else {
-    
+    echo "Failed\n";
 }
