98c4750e6121ef0a2b1ccc127ba175ebc1ce60c2
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-08 07:20:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ function soot_start() {
 //********************//
 $default_password = 'buyung_upik123';
 //$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50";
-$useragent = "Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8F191 Safari/6533.18.5";
+//$useragent = "Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8F191 Safari/6533.18.5";
 //********************//
 
 
-    $get_ip     = curl_attr('https://ipwhois.app/json/', false, false, false, false);
+    $get_ip     = curl_attr('https://ipwhois.app/json/');
     if($get_ip !== false) {
         $res_ip = json_decode($get_ip, true);
         echo "\033[1;37m========================================\033[1;37m\n";
@@ -28,8 +28,8 @@ $useragent = "Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/
     }
 
 
-    $get_attr   = curl_attr('https://randomuser.me/api/?gender=female&nat=us', false, false, false, false);
-    $get_email  = curl_attr('http://ese.kr/?pb=6549', false, false, false, false);
+    $get_attr   = curl_attr('https://randomuser.me/api/?gender=female&nat=us');
+    $get_email  = curl_attr('http://ese.kr/?pb=6549');
 
     if($get_attr !== false && $get_email !== false && strpos($get_email, 'name="mailbox"') !== false) {
         $res_attr   = json_decode($get_attr, true);
@@ -41,24 +41,9 @@ $useragent = "Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/
         echo "\033[1;37m◆ Email          : " . $email . "\033[1;37m\n";
         echo "\033[1;37m◆ Proxy Cookies  : ";
 
-        $get_accept_cookies = curl_attr('https://mbasic.facebook.com/reg/submit/?cid=103', false, $useragent, true, false);
-        if($get_accept_cookies !== false  && strpos($get_accept_cookies, 'method="post" action="https://mbasic.facebook.com/reg/submit/') !== false) {
-            echo "\033[1;32mTrue\033[1;37m\n";
-            echo "\033[1;37m◆ Create Account : ";
-
-            $reg_instance = replace_string('name="reg_instance" value="', '"', $get_accept_cookies);
-            $reg_impression_id = replace_string('name="reg_impression_id" value="', '"', $get_accept_cookies);
-
-            $data_post_fb_1 = 'lsd=AVpJP2d9jTM&jazoest=2891&ccp=2&reg_instance=4wd3Yk7MaQnfKf9NMX-LyXCQ&helper=&reg_impression_id=41104560-8378-4371-8d16-240c495d5bc7&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names[]=firstname&field_names[]=reg_email__&field_names[]=sex&field_names[]=birthday_wrapper&field_names[]=reg_passwd__&firstname='.$first_name.'&lastname='.$last_name.'&reg_email__='.$email.'&name=1&custom_gender=&did_use_age=false&birthday_day=11&birthday_month=10&birthday_year=2000&age_step_input=&reg_passwd__=badaklepas&submit=Sign Up';
-            $post_fb_1 = curl_attr('https://mbasic.facebook.com/reg/submit/?cid=103', $data_post_fb_1, $useragent, false, false);
-            echo $post_fb_1;
-        } else if($get_accept_cookies !== false && strpos($get_accept_cookies, 'method="post" action="/cookie/consent/') !== false) {
-            echo "\033[1;31mProxy Cookies Error\033[1;37m\n";
-            soot_start();
-        } else {
-            echo "\033[1;31mFailed...\033[1;37m\n";
-            soot_start();
-        }
+        $data_post_fb_1 = 'lsd=AVpJP2d9jTM&jazoest=2891&ccp=2&reg_instance=4wd3Yk7MaQnfKf9NMX-LyXCQ&helper=&reg_impression_id=41104560-8378-4371-8d16-240c495d5bc7&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names[]=firstname&field_names[]=reg_email__&field_names[]=sex&field_names[]=birthday_wrapper&field_names[]=reg_passwd__&firstname='.$first_name.'&lastname='.$last_name.'&reg_email__='.$email.'&name=1&custom_gender=&did_use_age=false&birthday_day=11&birthday_month=10&birthday_year=2000&age_step_input=&reg_passwd__=badaklepas&submit=Sign Up';
+        $post_fb_1 = curl_attr_fb('https://mbasic.facebook.com/reg/submit/?cid=103', $data_post_fb_1);
+        echo $post_fb_1;
     } else {
         soot_start();
     }
@@ -70,27 +55,39 @@ function replace_string($start, $end, $data) {
     return $rt;
 }
 
-function curl_attr($url, $body, $useragent, $createcookies, $readcookies) {
+function curl_attr($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'content-type: application/json',
-        'user-agent: ' . $useragent
-    ));
-    if($body) {
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-    }
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 25);
-    if($createcookies) {
-        curl_setopt($ch, CURLOPT_COOKIEJAR, 'tmp.txt');
+    $respons_data = curl_exec($ch);
+    $respons_header = substr($respons_data, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $respons_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    if($respons_http_code == 200) {
+        return $respons_data;
+    } else {
+        return false;
     }
-    if($readcookies) {
-        curl_setopt($ch, CURLOPT_COOKIEFILE, 'tmp.txt');
-    }
+}
+
+function curl_attr_fb($url, $body) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'content-type: application/json',
+        'user-agent: Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8F191 Safari/6533.18.5'
+    ));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 25);
     $respons_data = curl_exec($ch);
     $respons_header = substr($respons_data, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
     $respons_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
