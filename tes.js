56025b129414febbee5b9b88abe68caf63a7eef5
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-15 07:33:06 +0700

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,5 +1,22 @@
 starting();
 
 function starting() {
-    console.log('Starting OK');
+    var get_ip = ajax_get('https://ipwhois.app/json/', '');
+    if(get_ip !== false) {
+        console.log('IP : ' + get_ip['ip'] + ' || Country : ' + get_ip['country']);
+    }
+}
+
+
+function ajax_get(url, params) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url + params, true);
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            return this.responseText;
+        } else {
+            return false;
+        }
+    };
+    xhr.send();
 }
